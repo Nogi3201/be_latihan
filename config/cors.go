87c9148f8121ed3e7d18e2386dfd8e9730678298
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2026-06-22 15:44:22 +0700

    push kata nawal

--- config/cors.go
+++ config/cors.go
@@ -3,6 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://belatihan-production-a189.up.railway.app",
+	"my-iqilpt53z-study3201.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
