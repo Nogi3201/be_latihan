bb84deba9ebcbe0af7c156b28e2d8db83caad61e
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2026-06-22 15:14:33 +0700

    save

--- config/cors.go
+++ config/cors.go
@@ -3,7 +3,6 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://belatihan-production-a189.up.railway.app",
-	"https://belatihan-production-a189.up.railway.app",
 }
 
 func GetAllowedOrigins() []string {
